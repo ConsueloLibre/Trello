a48eb9dad2519019723960bebc3d67647ef556bf
ConsueloLibre <consuelomariana.acevedo@gmail.com>
2017-11-14 17:45:04 -0300

    actualizando por fail

---
+++ README.md
@@ -0,0 +1,10 @@
+*Reto de código DOM*
+
+**Entrampada en un error**
+Siento que he perdido horas intentando resolver el error que no me permite avanzar, para continuar con la pagina.
+
+**El gran error, "planificación"**
+Al no avanzar mucho durante la semana, no he posidio resolver esto por mi cuenta, si en la semana le hubiera dado mas tiempo, seguramente hubiera resulto el error en clases, con la ayuda de una compañera o profesora, ... (aun hAy tiempo)
+
+**Lo que mas me gustó**
+Sin duda ver mi avance en css, me resulta bastante divertido y simple el hecho de decorar y pseudo dominar. <3
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,51 @@
-function (add){
-  var addList=document.getElementsById('barraDeTexto');
-  var table= document.getElementById('comment').value; //tablero contenedor
-  document.getElementById('comment').value=""; //limpiar el valor del contenido luego de reservarlo
-  var newComment=document.createElement('div')
+function trello(){//la que hace el click
+  var contiene=document.getElementById('contiene'); //llamamos al padre, lo creamos, en donde se va a encontrar
+  var button= document.createElement('button');//crear boton que hace la funcion
+  var input=document.createElement('input'); // crear cajita que contendra texto Ingresado
+  var nodoText=document.createTextNode('Guardar');// el texto que contendra
 
-  var txt = document.getElementById('p');// texto que ira dentro del tablero contenedor
-  var contenedor= document.getElementById('contenedor'); //nuevo recuadro a la derecha que arrojara nuetra tarea.
+  //atributos
+  input.setAttribute('id','valor');
+  //asociar a la familia se crean los nodos
+  button.appendChild(nodoText);
+  contiene.appendChild(button);
+  contiene.appendChild(input);
 
-  var nodoText=document.createTextNode(addList);
+  //logica de orden en DOM
+//ahora el texto ingresado se guardara luego de presionar el "guardar"
+ button.addEventListener('click',function(){
+    var guardado=document.getElementById('valor').value; // con esto rescatamos el valor del input "guardar"
+    document.getElementById('valor').value="";// para limpiar input
+    var cajaContiene=document.getElementById('cajaContiene');//llamar a div para que lo guarde
 
-  //ahora que tenemos la familia comenzamos a assignar su parentesco
-  txt.appendChild(nodoText);
-  newComment.appendChild(txt);
-  Almacenado.appendChild(newComment);
+    var parrafo=document.createElement('p');//lo que se rescata se transforma en nodo de texto, para que exista
+    var textNuevo=document.createTextNode('guardado');//texto del input pasa al parrafo
+
+    parrafo.appendChild(textNuevo);//el texto pasa a ser hijo del parrafo
+    cajaContiene.appendChild(parrafo);// el parrafo es hijo de cajaContiene
+
+    var add=document.createElement('a');  // desde aca crea al elemento a
+    add.setAttribute=('href','#');
+    var textAdd=document.createTextNode('Añadir tarjeta');// se agraga el texto al nuevo elemento
+
+    add.appendChild(textAdd); //text add es hijo de elemento a
+    cajaContiene.appendChild(add);
+
+    //hasta aca crea todo para añadir tarjeta
+    add.addEventListener('click', function() {// al hacer click en agregar tarjeta estoy realizando el evento
+      var textarea=document.createElement('textarea');//creo un textarea
+      textarea.setAttribute('id','cuadro');
+
+      cajaContiene.appendChild(textarea);//textarea es hijo de caja contien
+
+      var add2=document.createElement('a');//un nuevo elemento para agregar una nueva tarjeta
+      add2.setAttribute('href','#');//otorgo nuevamente los atributos
+      var textarea2=document.createTextNode('Añadir Tarjeta');
+
+      add2.appendChild(textarea2); // textarea2 es hijo de a
+      cajaContiene.appendChild(add2);
+
+    })
+  })
+
 }
